7420e42e41cfe404b6e080b7e9ea2117d586d6a5
magnowerbett <magnowerbett@gmail.com>
2023-12-21 13:23:27 -0300

    att3

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,41 @@ function validateCadastro() {
     errorMessageElement.textContent = '';
     return true;
 }
+
+//Botão de Busca, página inicial
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const searchInput = document.querySelector('.form-control');
+        const searchButton = document.querySelector('.btn-outline-light');
+        const cardsContainer = document.querySelector('.row'); // Substitua por um seletor mais específico se necessário
+
+        // Função para filtrar e mostrar cards
+        function filterCards() {
+            const searchTerm = searchInput.value.toLowerCase();
+            const cards = cardsContainer.querySelectorAll('.card');
+
+            cards.forEach(card => {
+                const title = card.querySelector('.card-title').textContent.toLowerCase();
+                if (title.includes(searchTerm)) {
+                    card.style.display = ''; // Mostra o card
+                } else {
+                    card.style.display = 'none'; // Esconde o card
+                }
+            });
+        }
+
+        // Evento de clique para o botão de busca
+        searchButton.addEventListener('click', function(event) {
+            event.preventDefault();
+            filterCards();
+        });
+
+        // Evento de input para restaurar a tela inicial
+        searchInput.addEventListener('input', function() {
+            if (!this.value) {
+                const cards = cardsContainer.querySelectorAll('.card');
+                cards.forEach(card => card.style.display = ''); // Mostra todos os cards
+            }
+        });
+    });
+
